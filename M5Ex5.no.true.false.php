f56c4b3ac5ee385f83f7209be6b2a83351f31485
aaron1986 <aaronbrucesmith86@gmail.con>
2015-12-09 09:47:04 +0000

    first commit

--- M5Ex5.no.true.false.php
+++ M5Ex5.no.true.false.php
@@ -1,11 +1,15 @@
 <?php
-class TextFunctions {
-    public static $string = 'Hello, World!';
-    static public function strtoupper() {
 
+class TextFunctions {
+    static public function changeCase($string, $upperCase) {
+        if( $upperCase) return strtoupper( $string );
+                else return mb_strtolower( $string );
+      
     }
 }
 
-echo strtoupper(TextFunctions::$string);
+$string = "Hello, World!\n";
 
-?>
+echo TextFunctions::changeCase($string, true);
+echo TextFunctions::changeCase($string, false);
+
